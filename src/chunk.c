9503cb51b3604d20bcd89ef258ffa2a81601230a
Igor-Goncharenko <21gis05ru@gmail.com>
2025-08-28 13:45:27 +0500

    chore: Better code for face append function

--- src/chunk.c
+++ src/chunk.c
@@ -1,8 +1,6 @@
 #include "chunk.h"
 
 #include <math.h>
-#include <stdio.h>
-#include <string.h>
 
 #define WORLD_GEN_FUNC(x, y) (8.0f * sin(0.125f * x) * sin(0.125f * y) + 10.0f)
 
@@ -51,21 +49,6 @@ static const float CUBE_VERTICES[] = {
     -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
      0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 };
-
-//static const unsigned CUBE_INDICES[] = {
-//     0,  1,  2,     /* front */
-//     3,  2,  1,
-//     4,  5,  6,     /* right */
-//     7,  6,  5,
-//     8,  9, 10,     /* top */
-//    11, 10,  9,
-//    12, 13, 14,     /* back */
-//    15, 14, 13,
-//    16, 17, 18,     /* left */
-//    19, 18, 17,
-//    20, 21, 22,     /* bottom */
-//    23, 22, 21
-//};
 // clang-format on
 
 enum CubeFace {
@@ -79,25 +62,32 @@ enum CubeFace {
 
 static void _append_face_vert_to_arr(float *vertices, unsigned *n_vertices, unsigned *indices,
                                      unsigned *n_indices, const int x, const int y, const int z,
-                                     enum CubeFace face) {
-    int vert_offset = 6 * 4 * (int)face;
+                                     const enum CubeFace face) {
+    const int vert_offset = (int)face * 24;
+    const unsigned base_n_vertex = *n_vertices / 6;
 
-    float local_coords[3] = {(float)x, (float)y, (float)z};
+    for (int i = 0; i < 4; i++) {
+        const int src_idx = vert_offset + i * 6;
+        const int dst_idx = *n_vertices + i * 6;
 
-    float face_vert[4 * 6];
-    memcpy(face_vert, &CUBE_VERTICES[vert_offset], 6 * 4 * sizeof(float));
+        vertices[dst_idx + 0] = CUBE_VERTICES[src_idx + 0] + (float)x;
+        vertices[dst_idx + 1] = CUBE_VERTICES[src_idx + 1] + (float)y;
+        vertices[dst_idx + 2] = CUBE_VERTICES[src_idx + 2] + (float)z;
 
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 3; j++) face_vert[i * 6 + j] += local_coords[j];
-
-    memcpy(&vertices[*n_vertices], face_vert, 6 * 4 * sizeof(float));
-
-    unsigned indices_template[6] = {0, 1, 2, 3, 2, 1};
-    for (int i = 0; i < 6; i++) {
-        indices[(*n_indices)++] = *n_vertices / 6 + indices_template[i];
+        vertices[dst_idx + 3] = CUBE_VERTICES[src_idx + 3];
+        vertices[dst_idx + 4] = CUBE_VERTICES[src_idx + 4];
+        vertices[dst_idx + 5] = CUBE_VERTICES[src_idx + 5];
     }
 
-    (*n_vertices) += 6 * 4;
+    indices[*n_indices + 0] = base_n_vertex + 0;
+    indices[*n_indices + 1] = base_n_vertex + 1;
+    indices[*n_indices + 2] = base_n_vertex + 2;
+    indices[*n_indices + 3] = base_n_vertex + 3;
+    indices[*n_indices + 4] = base_n_vertex + 2;
+    indices[*n_indices + 5] = base_n_vertex + 1;
+
+    *n_indices += 6;
+    *n_vertices += 24;
 }
 
 static void _add_visible_faces(const struct Chunk *chunk, float *vertices, unsigned *n_vertices,
@@ -105,25 +95,19 @@ static void _add_visible_faces(const struct Chunk *chunk, float *vertices, unsig
                                const int z) {
     if (!chunk->data[z][y][x]) return;
 
-    if (x + 1 >= CHUNK_SIZE || !chunk->data[z][y][x + 1]) {
+    if (x + 1 >= CHUNK_SIZE || !chunk->data[z][y][x + 1])
         _append_face_vert_to_arr(vertices, n_vertices, indices, n_indices, x, y, z, FACE_RIGHT);
-    }
-    if (y + 1 >= CHUNK_SIZE || !chunk->data[z][y + 1][x]) {
+    if (y + 1 >= CHUNK_SIZE || !chunk->data[z][y + 1][x])
         _append_face_vert_to_arr(vertices, n_vertices, indices, n_indices, x, y, z, FACE_BACK);
-    }
-    if (z + 1 >= CHUNK_SIZE || !chunk->data[z + 1][y][x]) {
+    if (z + 1 >= CHUNK_SIZE || !chunk->data[z + 1][y][x])
         _append_face_vert_to_arr(vertices, n_vertices, indices, n_indices, x, y, z, FACE_TOP);
-    }
 
-    if (x - 1 < 0 || !chunk->data[z][y][x - 1]) {
+    if (x - 1 < 0 || !chunk->data[z][y][x - 1])
         _append_face_vert_to_arr(vertices, n_vertices, indices, n_indices, x, y, z, FACE_LEFT);
-    }
-    if (y - 1 < 0 || !chunk->data[z][y - 1][x]) {
+    if (y - 1 < 0 || !chunk->data[z][y - 1][x])
         _append_face_vert_to_arr(vertices, n_vertices, indices, n_indices, x, y, z, FACE_FRONT);
-    }
-    if (z - 1 < 0 || !chunk->data[z - 1][y][x]) {
+    if (z - 1 < 0 || !chunk->data[z - 1][y][x])
         _append_face_vert_to_arr(vertices, n_vertices, indices, n_indices, x, y, z, FACE_BOT);
-    }
 }
 
 void chunk_init(struct Chunk *chunk, const int x, const int y, const int z) {
@@ -173,8 +157,4 @@ void chunk_mesh_update(struct Chunk *chunk) {
     }
 
     upload_mesh_data(&chunk->mesh, vertices, n_vertices, indices, n_indices);
-
-    printf("%s [%d %d %d]: mesh update: %u vertices, %u indices\n", __func__, chunk->x, chunk->y,
-           chunk->z, n_vertices, n_indices);
-    fflush(stdout);
 }
